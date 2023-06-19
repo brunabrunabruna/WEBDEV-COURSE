2cd5204930630237f24150ac6b35956c68256267
brunabrunabruna <34475264+brunabrunabruna@users.noreply.github.com>
2023-06-19 16:09:09 +0200

    react intro

---
+++ SECTION_62_REACT_VITE/Airbnb/AirbnbComponent/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import PropertyList from "./PropertyList";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Airbnb Properties</h1>
+
+        <PropertyList properties={properties} />
+      </div>
+    </>
+  );
+}
+
+const properties = [
+  { id: 129031, name: "Desert Yurt", rating: 4.9, price: 150 },
+  { id: 129331, name: "Lone Mountain Cabin", rating: 4.8, price: 250 },
+  { id: 129032, name: "Cactus Retreat", rating: 4.75, price: 300 },
+  { id: 129033, name: "Redwood Treehouse Escape", rating: 4.9, price: 120 },
+  { id: 129034, name: "Oceanview Condo", rating: 4.7, price: 140 },
+  { id: 129035, name: "Gold Miner Campground", rating: 4.69, price: 96 },
+];
+
+export default App;
